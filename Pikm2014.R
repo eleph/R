2e463da01c3a597aa107bfe69803bb7756465926
eleph <znabao@gmail.com>
2015-06-03 14:41:58 +0900

    R code for Two Phases Outlier Detection in Different Subspaces
    Two Phases Outlier Detection in Different Subspaces
    
    @inproceedings{Bao:2014:TPO:2663714.2668046,
    author = {Bao, Zhana and Kameyama, Wataru},
    title = {Two Phases Outlier Detection in Different Subspaces},
    booktitle = {Proceedings of the 7th Workshop on Ph.D Students},
    series = {PIKM '14},
    year = {2014},
    isbn = {978-1-4503-1481-7},
    location = {Shanghai, China},
    pages = {57--62},
    numpages = {6},
    url = {http://doi.acm.org/10.1145/2663714.2668046},
    doi = {10.1145/2663714.2668046},
    acmid = {2668046},
    publisher = {ACM},
    address = {New York, NY, USA},
    keywords = {connected subspace, dimensional projection, high dimension,
    outlier score},
    }

---
+++ Pikm2014.R
@@ -0,0 +1,108 @@
+library(ROCR)
+dm = 100
+pn = 500
+K1=10
+K2 = 4
+#K2 < K1 * 2/3
+lp=5  #映射维数
+#k = 5+ceiling(runif(1)*5)
+nnd <-array(0, c((K1+1),pn))   # 邻居集
+knn <-array(0, c((K1+1),pn))
+denk <-array(0, c((K1+1)))
+
+den <-array(0, c(pn,dm))
+dev <-array(0, c(pn,dm))
+
+rdm <- c(1:dm)
+
+dat<- read.table("ArtiData100500.dat", sep=" ")
+dat<- as.matrix(dat)
+
+
+tim1<- proc.time()
+
+for(j in 1:dm)
+{
+ 
+for(i in 1:pn)
+{
+  #diff <- abs(scale(dat[,j],dat[i,j],FALSE))
+  dif <- abs(dat[,j]-dat[i,j])    
+  order.dist <- order(dif) #dtmp
+  orddist <- order.dist[1:(K1+1)]
+  nnd[,i] <- orddist
+  orddist <- orddist[-1]
+  tmp <- dif[orddist]
+  
+  den[i,j] <- (mean(tmp)+ tmp[K1])/2
+   
+}
+# 映射 密度
+ den[,j] <-mean(den[,j])/den[,j] 
+ #den[den[,j]>1,j]=1
+ 
+ 
+ rdm <- c(1:dm)
+ rdm <- rdm[-j]
+ rd <- sample(rdm,size=lp)     # 维数大于5时
+ #id <-which(rdm==rd)
+ #rdm <- rdm[-id]
+ 
+
+ for(v in 1:pn)
+	{
+   if(den[v,j]<0.25)       # too sparse to be deviated
+     {dev[v,j]=0
+      next}
+   for(u in rd)
+   {   
+    knn <- dat[nnd[,v],u]
+    for(x in 1:(K1+1))
+    {
+     #diff <- abs(scale(knn[],knn[x],FALSE))
+     dif <- abs(knn-knn[x])
+     #order.dist <- order(diff) #dtmp
+     #orddist <- order.dist[2:(K2+1)]
+     #tmp <- diff[orddist]
+     #denk[x] <- (mean(tmp)+ tmp[K2])/2
+     tmp <- sort(dif)
+     denk[x] <- (mean(tmp[2:(K2+1)])+ tmp[(K2+1)])/2
+    }
+    denk[1] <-mean(denk[])/denk[1]
+    print(denk)
+    #if(denk[1]>1)denk[1]<-1
+    dev[v,j] <- dev[v,j] + log2(denk[1])
+   } #end u
+   dev[,j] <- dev[,j] 
+  
+  }  #end v 
+
+}  # end j
+ 
+SI <- sapply(1:pn, function(i)(-1)*(sum(log2(den[i,]))+sum(dev[i,]))/dm)
+
+
+
+tim2<- proc.time()
+tim <- tim2-tim1
+print(tim)
+
+
+SIod <- rev(order(SI))
+outlier <- SI[SIod]
+output <- cbind(outlier,SIod)
+print(output[1:20,])
+
+
+st<-paste("ArtiRst",dm,pn,".dat", sep = "")
+z<-pn-10
+rst<-output
+rst[,2]=0
+rst[output[,2]>z,2]=1
+write.table(rst,file=st, row.names=F,col.names =F)
+
+
+
+
+
+
